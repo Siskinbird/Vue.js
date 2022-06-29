d730e7d5bff8ef9df44698320c3fe57c8a548d7b
Dmitrii Chizhov <dimachig@gmail.com>
2022-06-29 12:40:11 +0300

    feat: some easy app's

--- main.js
+++ main.js
@@ -4,3 +4,40 @@ let app = new Vue({
         message: "Hello VUE"
     }
 })
+
+let app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: "Page load at" + new Date().toLocaleString()
+    }
+})
+
+let app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })
+
+  let app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: "Интересно"}, 
+            {text: "Что из этого выйдет"},
+            {text: "Выглядит довольно просто"},
+            {text: "Думаю я справлюсь с этим дерьмом"}
+        ]
+    }
+  })
+  let app5 = new Vue({
+    el: '#app-5',
+    data: {
+        messageRev: 'Вот это мы перевернули',
+    },
+    methods: {
+        reverseMessage: function () {
+            this.messageRev = this.messageRev.split('').reverse().join('')
+        }
+    }
+  })
